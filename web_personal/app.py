6abb86b99fa837fe25cba94c8fd0a4c3ff4e34a8
Ma Guadalupe Montes <guadalupemontiel738@gmail.com>
2022-11-23 05:46:13 +0100

    Creando templates y rutas auth

--- web_personal/app.py
+++ web_personal/app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+########### Rutas Public #########
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('public/index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('public/about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('public/contact.html')
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    return render_template('public/portfolio.html')
+
+####### Rutas #######
+
+@app.route('/auth/login')
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/auth/register')
+def register():
+    return render_template('auth/register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
